2003c783487cce8f3fa6ac85c6016f6a400dd1cb
Khali Gopaul <khaligopaul@Khalis-MacBook-Air.local>
2022-07-14 10:44:11 -0400

    created homework folder

---
+++ homework.js
@@ -0,0 +1,28 @@
+// 1. Write a function that adds two numbers together.
+// 2. Write a car object that has a modal and color.  Create a property called year then console.log all of the objects properties individually.
+// 3. Write a function to get the first element in an array.
+
+function sum(x,y) {
+    return (x + y)
+}
+console.log(sum(2,3))
+console.log(sum(5,2))
+console.log(sum(5,8))
+
+
+let car = {
+
+    model: "Jeep",
+    colour: "Red",
+    year = '2010',
+}
+    console.log(car.model),
+    console.log(car.colour),
+    console.log(car.year),
+
+
+    let MyFavouritethings = ['dogs', 'coffee', 'vacations']
+
+    console.log(getElement(MFavouritethings))
+
+
